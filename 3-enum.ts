190cc373ba53ab4ef56e6f0c49393f53235bc4d7
Vlad Gonchar <vladik.gonchar2015@gmail.com>
2021-09-06 00:08:09 +0300

    start date TypeScript

---
+++ 3-enum.ts
@@ -0,0 +1,21 @@
+enum Membership {
+    Simple,
+    Standart,
+    Premium,
+}
+
+const membership = Membership.Standart;
+const membershipReverse = Membership[2];
+console.log(membershipReverse);
+
+enum UserResponse {
+    No = 0,
+    Yes = 1,
+}
+
+function respond(recipient: string, message: UserResponse): void {
+    console.log(recipient);
+    console.log(message);
+}
+
+respond('Princess Caroline', UserResponse.Yes);
